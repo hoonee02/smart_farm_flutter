7c1a9de25cc433a0ea5f0a536c47e405124d4ab2
심성훈 <hoonee02@outlook.com>
2023-01-11 14:49:01 +0900

    sensorPage display1

--- lib/SensorPage.dart
+++ lib/SensorPage.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class SensorPage extends StatelessWidget {
+
+  void getdata(){
+    final uri = Uri(
+      scheme: 'http',
+      host:'192.168.1.43',
+      port: 5001,
+      path: '/showapp',
+
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints){
       return Column(
         children: [
+          Container(
+            margin: EdgeInsets.all(5),
+            width: constraints.maxWidth - 10,
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('일시'),
+                Text(),
+              ],
+            ),
+          ),
           Row(
             children: [
               Container(
@@ -25,6 +59,7 @@ class SensorPage extends StatelessWidget {
                       height: 10,
                     ),
                     Text('온도'),
+                    Text(),
                   ],
                 ),
               ),
