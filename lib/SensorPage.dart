fab273730cc9b51891262f1efcb6a7d4ba170b2c
심성훈 <hoonee02@outlook.com>
2023-01-12 15:30:00 +0900

    강사님 도움

--- lib/SensorPage.dart
+++ lib/SensorPage.dart
@@ -1,26 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-class SensorPage extends StatelessWidget {
+class SensorPage extends StatefulWidget {
+  const SensorPage({super.key});
 
-  void getdata(){
-    final uri = Uri(
-      scheme: 'http',
-      host:'192.168.1.43',
-      port: 5001,
-      path: '/showapp',
-
-  );
+  @override
+  State<SensorPage> createState() => _SensorPageState();
 }
+
+class _SensorPageState extends State<SensorPage> {
+  String date = "";
+  int temperature = 0;
+  int humidity = 0;
+
+  void getData() async {
+    print('start getData');
+    final url = Uri.parse('http://192.168.1.43:5001/showapp');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+      convert.jsonDecode(response.body);
+      // var itemCount = jsonResponse['totalItems'];
+      print('Number of books about http: ${jsonResponse[0][0][0]}.');
+      print('Number of books about http: ${jsonResponse[1][0][0]}.');
+      print('Number of books about http: ${jsonResponse[2][0][0]}.');
+
+      temperature = int.parse(jsonResponse[0][0][0].toString());
+      humidity = int.parse(jsonResponse[1][0][0].toString());
+      date = jsonResponse[2][0][0].toString();
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints){
+    return LayoutBuilder(builder: (context, constraints) {
       return Column(
         children: [
           Container(
-            margin: EdgeInsets.all(5),
+            margin: const EdgeInsets.all(5),
             width: constraints.maxWidth - 10,
             height: 100,
             decoration: BoxDecoration(
@@ -28,22 +49,23 @@ class SensorPage extends StatelessWidget {
                 color: Colors.blue,
                 width: 5,
               ),
-              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
             ),
             child: Column(
               children: [
                 const SizedBox(
                   height: 10,
                 ),
-                Text('일시'),
-                Text(),
+                const Text('일시'),
+                const SizedBox(height: 10,),
+                Text(date, style: const TextStyle(fontSize: 20, color: Colors.green),),
               ],
             ),
           ),
           Row(
             children: [
               Container(
-                margin: EdgeInsets.all(5),
+                margin: const EdgeInsets.all(5),
                 width: constraints.maxWidth / 2 - 10,
                 height: 100,
                 decoration: BoxDecoration(
@@ -51,15 +73,16 @@ class SensorPage extends StatelessWidget {
                     color: Colors.blue,
                     width: 5,
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
                 ),
                 child: Column(
                   children: [
                     const SizedBox(
                       height: 10,
                     ),
-                    Text('온도'),
-                    Text(),
+                    const Text('온도'),
+                    const SizedBox(height: 10,),
+                    Text('$temperature', style: const TextStyle(fontSize: 30, color: Colors.red),),
                   ],
                 ),
               ),
@@ -71,19 +94,27 @@ class SensorPage extends StatelessWidget {
                     color: Colors.blue,
                     width: 5,
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
                 ),
                 child: Column(
                   children: [
                     const SizedBox(
                       height: 10,
                     ),
-                    Text('습도'),
+                    const Text('습도'),
+                    const SizedBox(height: 10,),
+                    Text('$humidity', style: const TextStyle(fontSize: 30, color: Colors.blue),),
                   ],
                 ),
               ),
             ],
           ),
+          TextButton(
+              onPressed: () {
+                print('refresh click');
+                getData();
+              },
+              child: const Text('새로고침')),
         ],
       );
     });
